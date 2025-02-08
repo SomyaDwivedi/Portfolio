b1722e923a242b8562247dd73f2532a37dda407a
Somya Dwivedi <somya.dwivedi@yahoo.com>
2025-02-08 16:06:21 -0500

    Add subtitle to Header component for enhanced clarity

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,12 @@
 import './Header.css';
 
 function Header() {
-    return <h1>Somya Dwivedi</h1>;
+    return (
+        <>
+            <h1>Somya Dwivedi</h1>
+            <br />
+            <h4>Network Engineer</h4>
+        </>
+    );
 }
-
 export default Header;
