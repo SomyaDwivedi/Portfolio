03a2b4e0df7adfcc40359ec9d79e1804edb807e3
Somya Dwivedi <somya.dwivedi@yahoo.com>
2025-02-17 21:13:51 -0500

    Fix typo in Projects link in Header component

--- src/Header.tsx
+++ src/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
             <nav>
                 <ul>
                     <li><a href="#about">About Me</a></li>
-                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#projects">Project</a></li>
                     <li><a href="#skills">Skills</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
